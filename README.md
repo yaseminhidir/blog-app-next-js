0a899befc7a8dac31d33f7225c5685308ea8dde2
unknown <yaseminhidir@gmail.com>
2023-04-17 21:55:26 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# #Blog
+
+This application is a simple blog website developed for learning SSR using Next.js with a Bootstrap theme, and the frontend is developed using React with a component-based approach.
+
+To focus on the project's purpose, it is developed with data provided from a JSON file instead of a database.
+
+## Screenshots
+
+![home](./screenshots/home.jpg)
+![profile](./screenshots/profile.jpg)
+![bloggers](./screenshots/bloggers.jpg)
+![categories](./screenshots/categories.jpg)
+![posts](./screenshots/posts.jpg)
+
--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,39 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
-export default function Home() {
+import Image from 'next/image';
+
+
+import Headline from '../components/Headline';
+import Layout from '../components/layout';
+import postService from '../services/postService';
+import { PostCard } from '../components/Posts/PostCard';
+
+
+export default function Home({ posts,randomPost }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <Layout>
+       <Headline data={randomPost}></Headline>
+      <div className="container mt-2 mb-2 pt-5 pb-5" id="article-grid">
+        <div className="row justify-content-center">
+          {
+            posts.map((post, index) => (
+              <PostCard post={post} key={index}></PostCard>
+            ))
+          }
         </div>
-      </main>
+        <div class="container text-center pb-3 mb-5">
+          <a href="/posts" class="btn btn-lg btn-light">More Articles</a>
+        </div>
+      </div>
+    </Layout>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
   )
+}
+export function getServerSideProps() {
+  var posts=postService.getPostList()
+  return {
+    props: {
+      posts: postService.getPostList().slice(0,2),
+      randomPost: posts[Math.floor(Math.random()*posts.length)]
+    },
+  }
 }
